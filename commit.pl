214388ad5e36546953f7a8ffc3dc62d58b823acf
Freising Denuevo <Freising Denuevo>
2016-08-11 12:23:25 +0200

    Init

---
+++ commit.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $name = shift @ARGV || die "\nLack of commit name\n\n";
+
+foreach my $arg ( @ARGV ) {
+  $name .= " $arg";
+}
+
+my @files = qw("commit.pl"
+	       "package.json"
+	       "server.js"
+	       "lib/*.js"
+	       # "views/*.handlebars"
+	       # "views/layouts/*.handlebars"
+	       # "public/*"
+	     );
+
+foreach my $file ( @files ) {
+  system("git add $file");
+}
+
+
+system("git commit -m '$name'\n");
