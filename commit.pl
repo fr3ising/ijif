1031777e77878c12172468953f12805640ecd34a
Freising Denuevo <Freising Denuevo>
2016-08-11 12:23:46 +0200

    Init

--- commit.pl
+++ commit.pl
@@ -12,10 +12,11 @@ my @files = qw("commit.pl"
 	       "package.json"
 	       "server.js"
 	       "lib/*.js"
-	       # "views/*.handlebars"
-	       # "views/layouts/*.handlebars"
-	       # "public/*"
 	     );
+
+# "views/*.handlebars"
+# "views/layouts/*.handlebars"
+# "public/*"
 
 foreach my $file ( @files ) {
   system("git add $file");
