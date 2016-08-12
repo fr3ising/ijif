79fcd715e4dcbb28590c196c7904ab63f7a5ba30
Freising Denuevo <Freising Denuevo>
2016-08-12 10:23:13 +0200

    Cookie maxAge

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(credentials.cookieSecret));
 app.use(session({
     secret: credentials.cookieSecret,
+    cookie: {maxAge:7*24*3600*1000},
     proxy: true,
     resave: true,
     store: new fileStore,
